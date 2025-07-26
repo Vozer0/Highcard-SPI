a4547bacc6abe377ad1139db1f76401344357ab9
David Vocero <vocerod004@gmail.com>
2025-07-25 21:43:38 -0700

    got rid of test servers

--- pico/main.py
+++ pico/main.py
@@ -11,7 +11,7 @@ def main():
         connect_internet("SM-Vozero", password="DVoce701204") #ssid (wifi name), pass
         
         # HiveMQ Cloud connection - Cluster 2
-        client = connect_mqtt("ae560d05cdca43359485c6741741a1e8.s1.eu.hivemq.cloud", "David2", "High_Card16")
+        client = connect_mqtt("09f1b50573094b6894b56ef79a8f1140.s1.eu.hivemq.cloud", "David2", "High_Card16")
         
         # Display connection success
         display_text("Connected!")
